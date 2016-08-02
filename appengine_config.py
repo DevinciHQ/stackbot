74a088470625006c1e5a57ba8abc72e7c040bdd9
Frank Carey <frank@devinci.io>
2016-08-02 13:54:24 -0400

    Create vendored python dependecy setup.
    - Used recommendation from google docs where all packages in lib are included with the app engine build.
    - Requires extra pip flag, so added ahoy pip as a command to ahoy.

---
+++ appengine_config.py
@@ -0,0 +1,6 @@
+# Solution for adding vendored libraries.
+# See https://cloud.google.com/appengine/docs/python/tools/using-libraries-python-27
+from google.appengine.ext import vendor
+
+# Add any libraries installed in the "lib" folder.
+vendor.add('lib')
