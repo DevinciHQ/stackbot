a951aa2c892e76d3adb851d2c63b6d5660c0a880
Frank Carey <frank@devinci.io>
2016-08-02 15:02:39 -0400

    Cleanup python code to meet PEP 8 standards.

--- main.py
+++ main.py
@@ -16,40 +16,39 @@ import webapp2
 
 # For use when dealing with the datastore.
 from google.appengine.ext import ndb
-
 from user_agents import parse as parseUA
+
 
 # A simple Datastore Entity that takes any values (good for development)
 # See https://cloud.google.com/appengine/docs/python/ndb/creating-entity-models
 class Query(ndb.Expando):
     pass
 
+
 # This handles the incoming request and creates a Query entity if a query string
 # was passed.
 class QueryHandler(webapp2.RequestHandler):
-  def get(self):
-    q = self.request.get('q')
-    # Make sure the length of the query string is at least 1 char.
-    if len(q) > 0:
-      # Create a new Query entity from the q value.
-      # TODO: Add the other values that we want from the request headers.
-      query = Query(query=q)
-      # Save to the datatore.
-      query.put()
+    def get(self):
+        q = self.request.get('q')
+        # Make sure the length of the query string is at least 1 char.
+        if len(q) > 0:
+            # Create a new Query entity from the q value.
+            # TODO: Add the other values that we want from the request headers.
+            query = Query(query=q)
+            # Save to the datatore.
+            query.put()
 
-      if self.request.headers['User-Agent']:
-        self.response.write(parseUA(self.request.headers['User-Agent']))
+            if self.request.headers['User-Agent']:
+                self.response.write(parseUA(self.request.headers['User-Agent']))
 
-      # Output some debug messages for now.
-      # TODO: Redirect to google.
-      self.response.write('Saved')
-      self.response.write(q)
+            # Output some debug messages for now.
+            # TODO: Redirect to google.
+            self.response.write('Saved')
+            self.response.write(q)
 
-    # Output for when we first land on the page (or when no query was entered)
-    self.response.headers['Content-Type'] = 'text/plain'
-    self.response.write('Yey!' + q)
-
-
+        # Output for when we first land on the page (or when no query was entered)
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.write('Yey!' + q)
 
 # Actually run the webserver and accept requests.
 app = webapp2.WSGIApplication([
