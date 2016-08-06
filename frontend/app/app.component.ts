534958467b7235c9890b4ec5545275eaad05608a
Frank Carey <frank@devinci.io>
2016-08-06 12:42:19 +0300

    Followed the angularfire setup instructions, but only up to 1.3. See https://github.com/angular/angularfire2/blob/master/docs/1-install-and-setup.md

--- frontend/app/app.component.ts
+++ frontend/app/app.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
+import { AngularFire, AuthProviders, AuthMethods } from 'angularfire2';
 
 @Component({
+  //moduleId: module.id,
   selector: 'my-app',
-  template: '<h1>My First Angular 2 App</h1>'
+  template: `
+  <div> {{ (af.auth | async)?.uid }} </div>
+  <button (click)="login()">Login With Twitter</button>
+  <button (click)="overrideLogin()">Login Anonymously</button>
+  `,
 })
-export class AppComponent { }
+export class AppComponent {
+  constructor(public af: AngularFire) {
+    this.af.auth.subscribe(auth => console.log(auth));
+  }
+  login() {
+    this.af.auth.login({
+      provider: AuthProviders.Twitter,
+      method: AuthMethods.Popup,
+    });
+  }
+  overrideLogin() {
+    this.af.auth.login({
+      provider: AuthProviders.Anonymous,
+      method: AuthMethods.Anonymous,
+    });
+  }
+}
