ab423bca1bc343a544f641096366544a9d473ef7
Aashil <aashil@devinci.io>
2016-08-11 17:34:03 -0400

    Refactor code so that redirect works. I based the fixes on debugging direcctly in chrome and this article https://coryrylan.com/blog/angular-2-observable-data-services .
    Refactor code so that redirect works. I based the fixes on debugging direcctly in chrome and this article https://coryrylan.com/blog/angular-2-observable-data-services .

--- frontend/src/app/search.component.ts
+++ frontend/src/app/search.component.ts
@@ -6,36 +6,35 @@
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { GetService } from "./get.service";
+import { RedirectService } from "./redirect.service";
 
 @Component({
     selector: 'search',
     templateUrl: './app/search.component.html',
-    providers: [GetService]
+    providers: [GetService, RedirectService]
     //styleUrls: ['app.component.css']
 })
 
 @Injectable()
 export class SearchComponent {
 
-    constructor(private getService: GetService){}
-    private data;
+    private data: any;
+    public redirectService: RedirectService;
+    public getService: GetService;
+
+    constructor(getService: GetService, redirectService: RedirectService){
+        this.redirectService = redirectService;
+        this.getService = getService;
+    }
 
     submit(searchField: string) {
         if(searchField != '') {
-            this.getService.getUrl(searchField)
-                .subscribe(
-                    function (data) {
-                        this.data = data
-                    },
-                    function (error) {
-                        console.log("Error happened: " + error)
-                    },
-                    function () {
-                        if (this.data.success) {
-                            window.location.href = this.data.payload.redirect;
-                        }
-                    }
-                );
+            this.getService.getUrl(searchField).subscribe(
+                data => {
+                    this.data = data
+                    this.redirectService.redirect(this.data);
+                }, error => { console.log("Error happened: " + error);}
+            );
         }
     }
 
