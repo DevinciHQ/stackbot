c5909140755f56584f29286821164890130467de
Aashil <aashil@devinci.io>
2016-08-12 13:24:02 -0400

    PROD-230: Write a empty ReportHandler for handling the GET requests made by the frontend to fetch the reports.
    * Also, indented the code properly to confirm the python standards.

--- backend/main.py
+++ backend/main.py
@@ -41,11 +41,13 @@ def get_geo_data(request):
 # This handles the incoming request and creates a Query entity if a query string
 # was passed.
 class QueryHandler(webapp2.RequestHandler):
+
     def options(self):
             # Output CORS headers for non-GET requests (json data POSTS)
             self.response.headers['Access-Control-Allow-Origin'] = '*'
             self.response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
             self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+
     def get(self):
         q = self.request.get('q')
         # Make sure the length of the query string is at least 1 char.
@@ -101,9 +103,24 @@ class QueryHandler(webapp2.RequestHandler):
             }
             self.response.out.write(json.dumps(output))
 
+
+class ReportHandler(webapp2.RequestHandler):
+
+    def options(self):
+        # Output CORS headers for non-GET requests (json data POSTS)
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+        self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+
+    def get(self):
+        # Output CORS headers for GET requests
+        self.response.headers['Access-Control-Allow-Origin'] = '*'
+        self.response.headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept'
+        self.response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+
 # Actually run the webserver and accept requests.
 app = webapp2.WSGIApplication([
-    ('/api/q', QueryHandler),
+    ('/api/q', QueryHandler), ('/api/report', ReportHandler)
 ], debug=True)
 
 
--- frontend/src/app/report.component.ts
+++ frontend/src/app/report.component.ts
@@ -2,7 +2,7 @@
  * Created by aashil on 8/12/16.
  */
 
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'report',
@@ -10,6 +10,10 @@ import { Component } from "@angular/core";
     styleUrls: ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u']
 })
 
-export class ReportComponent{
+export class ReportComponent implements OnInit{
+
+    ngOnInit() {
+        console.log('testing the on init');
+    }
 
 }
