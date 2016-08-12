83b8f383f6310a6ec67818a0839249bd7e0c8b39
Aashil <aashil@devinci.io>
2016-08-12 12:49:02 -0400

    PROD-230: Add bootstrap cdn in the styleUrls to use it in our HTML template.

--- frontend/src/app/report.component.ts
+++ frontend/src/app/report.component.ts
@@ -6,7 +6,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'report',
-    templateUrl: './app/report.component.html'
+    templateUrl: './app/report.component.html',
+    styleUrls: ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u']
 })
 
 export class ReportComponent{
