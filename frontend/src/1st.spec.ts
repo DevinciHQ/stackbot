59d209837dc95acda95a8109056444ad4f56d073
Aashil <aashil@devinci.io>
2016-08-15 14:35:14 -0400

    PROD-224: Fix me for the tests.

--- frontend/src/1st.spec.ts
+++ frontend/src/1st.spec.ts
@@ -1,6 +1,9 @@
 /**
  * Created by aashil on 8/15/16.
  */
+import { SearchComponent } from './app/search.component'
+import { GetService } from './app/get.service'
+import { RedirectService } from './app/redirect.service'
 
 describe('1st tests', () => {
     it('true is true', () => expect(true).toEqual(true));
@@ -8,3 +11,19 @@ describe('1st tests', () => {
         () => expect(null).not.toEqual(undefined)
     );
 });
+
+describe('Submit', () => {
+    beforeEach(() => {
+        this.searchComponent = new SearchComponent();
+    });
+    spyOn(this.searchComponent, "submit");
+    it("Submit method has been called", () => expect(this.searchComponent.submit("hello")).toHaveBeenCalled());
+});
+
+describe('Service: LanguagesService', () => {
+    beforeEachProviders(() => [LanguagesService]);
+
+    it('contains English', inject([SearchComponent], (search) => {
+        expect(search.submit());
+    }));
+})
