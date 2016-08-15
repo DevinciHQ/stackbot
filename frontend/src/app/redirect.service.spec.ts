6699da8b674b217c922c3a56ea385658ffaf5305
Aashil <aashil@devinci.io>
2016-08-15 19:06:50 -0400

    PROD-224: Write a test for redirect

--- frontend/src/app/redirect.service.spec.ts
+++ frontend/src/app/redirect.service.spec.ts
@@ -2,15 +2,11 @@
  * Created by aashil on 8/15/16.
  */
 
-import { Hero } from './hero';
-describe('Hero', () => {
+import { RedirectService } from './redirect.service';
+
+describe('redirect', () => {
     it('has name', () => {
-        let hero: Hero = {id: 1, name: 'Super Cat'};
-        expect(hero.name).toEqual('Super Cat');
-    });
-    it('has id', () => {
-        let hero: Hero = {id: 1, name: 'Super Cat'};
-        expect(hero.id).toEqual(1);
+        let redirect: RedirectService = {};
     });
 });
 
