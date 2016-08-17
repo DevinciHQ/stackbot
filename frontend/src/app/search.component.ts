2fa0c85379050d9846f76a0323adc129c1b70358
Frank Carey <frank@devinci.io>
2016-08-17 11:02:16 -0400

    Fix linting errors produced by 'ahoy test'.

--- frontend/src/app/search.component.ts
+++ frontend/src/app/search.component.ts
@@ -1,18 +1,14 @@
-/**
- * Created by aashil on 8/9/16.
- */
-
 
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
-import { GetService } from "./get.service";
-import { RedirectService } from "./redirect.service";
+import { GetService } from './get.service';
+import { RedirectService } from './redirect.service';
 
 @Component({
     selector: 'search',
     templateUrl: './app/search.component.html',
     providers: [GetService, RedirectService]
-    //styleUrls: ['app.component.css']
+    // styleUrls: ['app.component.css']
 })
 
 @Injectable()
@@ -20,21 +16,21 @@ export class SearchComponent {
 
     private data: any;
 
-    constructor(private getService: GetService, private redirectService: RedirectService){}
+    constructor(private getService: GetService, private redirectService: RedirectService) {}
 
     submit(searchField: string) {
-        if(searchField != '') {
+        if (searchField !== '') {
             this.getService.getUrl(searchField).subscribe(
                 data => {
                     this.data = data;
                     this.redirectService.redirect(this.data);
-                }, error => { console.log("Error happened: " + error);}
+                }, error => { console.log('Error happened: ' + error); }
             );
         }
     }
 
-    onPressEnter(e, searchField){
-        if(e.keyCode == 13 && searchField != ''){
+    onPressEnter(e, searchField) {
+        if (e.keyCode === 13 && searchField !== '') {
             this.submit(searchField);
         }
     }
