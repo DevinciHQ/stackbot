c7b677870487c72e0502d6a4f40e3168982befe1
Frank Carey <frank@devinci.io>
2016-08-17 15:26:28 -0400

    PROD-224: Refactor: Finish the refactoring of QueryService.
    - It now handles redirects on it's own.
    - It has helper methods for making backend requests to multiple api endpoints.
    - SearchComponent is now much more simple.
    - It automatically subscribes to firebash auth so that when someone logs in and out, uid is updated.

--- frontend/src/app/query.service.ts
+++ frontend/src/app/query.service.ts
@@ -1,36 +1,82 @@
-/**
- * Created by aashil on 8/10/16.
- */
 
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
+import { AngularFire } from 'angularfire2';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class QueryService {
 
-    private backendUrl = 'http://localhost:8081/api/q?q=';  // URL to web API
-    constructor (private http: Http) {}
+    private uid: string = null;
+    private backendUrl = 'http://localhost:8081';  // URL to web API
+
+    constructor (private http: Http, private af: AngularFire) {
+
+        // Subscribe to the authentication events of angularfire2 so we can update
+        // our local version of the UID when appropriate.
+        this.af.auth.subscribe(
+            auth => {
+                if (auth != null) {
+                    this.uid = auth.uid;
+                } else {
+                    this.uid = null;
+                }
+            }
+        );
+    }
+
     // Save the search query and get the JSON response (which also contains link to redirect) in return.
-    getUrl (searchField: String, uid: String): Observable<JSON> {
-        if (uid !== undefined) {
-            return this.http.get(this.backendUrl + searchField + '&uid=' + uid)
-                .map(this.extractData)
-                .catch(this.handleError);
-        } else {
-            return this.http.get(this.backendUrl + searchField)
-                .map(this.extractData)
-                .catch(this.handleError);
-        }
+    public doQuery(query: string) {
+        this._backendRequest('/api/q', {q: query }).subscribe(
+            data => {
+                // If when data is returned from a query with a redirect set, do the redirect.
+                if (data['redirect']) {
+                    this._redirect(data['redirect']);
+                }
+            }
+        );
     }
+
     // Extract the JSON data from the response object.
-    private extractData(res: Response) {
-        let body = res.json();
-        return body || { };
+    private _backendRequest(endpoint: string, data: { [ key: string ]: string; }): Observable<Object> {
+        let getParams: string[] = [];
+
+        if (this.uid !== undefined) {
+            data['uid'] = this.uid;
+        }
+        let requestUrl = this.backendUrl + endpoint;
+        Object.keys(data).forEach(function(key) {
+            getParams.push(key + '=' + data[key]);
+        });
+        if (getParams) {
+            requestUrl += '?' + getParams.join('&');
+        }
+
+        return this.http.get(requestUrl)
+                .map(this._extractData)
+                .catch(this._handleError);
     }
-    // Handling the errors in case the GET request above throws an error.
-    private handleError (error: any) {
+
+    private _redirect(href) {
+        window.location = href;
+    }
+
+    /**
+     * Process the returned data that will be handed off to the requesting element.
+     */
+    private _extractData(res: Response) {
+        let body = res.json();
+        // Ensure that the response was OK and that the success in the body of the JSON response was true.
+        if (!res.ok || !body['success'] === true) {
+            console.log('Request to ' +  res.url + ' (query service) failed.');
+            return {};
+        }
+        return body['payload'] || { };
+    }
+
+ // Handling the errors in case the GET request above throws an error.
+    private _handleError (error: any) {
         // In a real world app, we might use a remote logging infrastructure
         // We'd also dig deeper into the error to get a better message
         let errMsg = (error.message) ? error.message :
--- frontend/src/app/search.component.ts
+++ frontend/src/app/search.component.ts
@@ -2,52 +2,28 @@
 import { Component } from '@angular/core';
 import { Injectable } from '@angular/core';
 import { QueryService } from './query.service';
-import { RedirectService } from './redirect.service';
-import { AngularFire } from 'angularfire2';
 
 @Component({
     selector: 'search',
     templateUrl: './app/search.component.html',
-    providers: [QueryService, RedirectService]
+    providers: [QueryService]
     // styleUrls: ['app.component.css']
 })
 
 @Injectable()
 export class SearchComponent {
 
-    private uid: String;
-
-    constructor(private getService: QueryService, private redirectService: RedirectService, private af: AngularFire) {}
+    constructor(private queryService: QueryService) {}
 
     submit(searchField: string) {
         if (searchField !== '') {
-
-            this.af.auth.subscribe(
-              auth => {
-                  if (auth != null) {
-                      this.uid = auth.uid;
-                  }
-                  this.getService.getUrl(searchField, this.uid).subscribe(
-                      data => {
-                          if (data['success'] !== true) {
-                              console.log('Backend returned false, don\'t redirect.');
-                              return;
-                          }
-                          if (typeof(data['payload']['redirect']) !== 'string') {
-                              console.log('Backend returned no redirect, don\'t redirect.');
-                              return;
-                          }
-                          this.redirectService.redirect(data['payload']['redirect']);
-                      }, error => { console.log('Error happened: ' + error); }
-                 );
-              }
-            );
+            this.queryService.doQuery(searchField);
         }
     }
 
     onPressEnter(e, searchField) {
         if (e.keyCode === 13 && searchField !== '') {
-            this.submit(searchField);
+            this.queryService.doQuery(searchField);
         }
     }
 }
