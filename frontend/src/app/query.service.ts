dddf155e906c9139fb3d379e0275aa3886868a96
Aashil <aashil@devinci.io>
2016-08-17 14:09:06 -0400

    PROD-224: Add helpful comments in the 'query.service.ts' file.

--- frontend/src/app/query.service.ts
+++ frontend/src/app/query.service.ts
@@ -12,7 +12,7 @@ export class QueryService {
 
     private backendUrl = 'http://localhost:8081/api/q?q=';  // URL to web API
     constructor (private http: Http) {}
-
+    // Save the search query and get the JSON response (which also contains link to redirect) in return.
     getUrl (searchField: String, uid: String): Observable<JSON> {
         if (uid !== undefined) {
             return this.http.get(this.backendUrl + searchField + '&uid=' + uid)
@@ -24,10 +24,12 @@ export class QueryService {
                 .catch(this.handleError);
         }
     }
+    // Extract the JSON data from the response object.
     private extractData(res: Response) {
         let body = res.json();
         return body || { };
     }
+    // Handling the errors in case the GET request above throws an error.
     private handleError (error: any) {
         // In a real world app, we might use a remote logging infrastructure
         // We'd also dig deeper into the error to get a better message
