4673fc48343bf7bd7a8c57cbe6fe9172b7e3ea10
Frank Carey <frank@devinci.io>
2016-08-18 13:13:28 -0400

    PROD-224: Update the QueryService tests so they pass (for now). Works by mocking AngularFireAuth.

--- frontend/src/app/query.service.spec.ts
+++ frontend/src/app/query.service.spec.ts
@@ -1,8 +1,50 @@
-/**
- * Created by aashil on 8/17/16.
- */
+
+import {BaseRequestOptions, Response, ResponseOptions, Http} from '@angular/http';
+import {addProviders, inject, async} from '@angular/core/testing';
+import { QueryService } from './query.service';
+import { AngularFireAuth, FirebaseAuthState, FIREBASE_PROVIDERS} from 'angularfire2';
+import { MockBackend, MockConnection} from '@angular/http/testing';
+
+class AuthBackend extends MockBackend {
+
+}
+
+
+class MockAngularFireAuth {
+
+  public login(): any {
+      // jasmine.crea
+     // new Promise();
+  }
+  public logout() {
+  }
+
+    public subscribe() {
+    }
+}
 
 describe('QueryService', () => {
-    it('should get the JSON data containing the URL when getURL is called', () => {
+    beforeEach(() => {
+        addProviders([
+            QueryService,
+            MockBackend,
+            BaseRequestOptions,
+            {provide: AngularFireAuth , useClass: MockAngularFireAuth},
+            {provide: Http, useFactory: (backend, options) => {
+              return new Http(backend, options);
+            }, deps: [MockBackend, BaseRequestOptions]},
+        ]);
     });
+
+    it('should get some data', inject([QueryService], (queryService: QueryService) => {
+
+    }));
+
+    /*it('should get the JSON data containing the URL when getURL is called',
+        inject([QueryService, Http, AngularFire], (queryService: QueryService, af: AngularFire) => {
+            spyOn(queryService, 'doQuery');
+            queryService.doQuery('hello');
+            expect(queryService.doQuery).toHaveBeenCalled();
+        })
+    );*/
 });
