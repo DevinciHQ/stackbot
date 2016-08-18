94cb0b8e5323fecd361fc0b235c07d1e53f53994
Aashil <aashil@devinci.io>
2016-08-18 10:26:17 -0400

    PROD-230: Add accidentally deleted import statements while rebasing off origin/master

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,3 +1,5 @@
+import {bootstrap} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from '@angular/core';
 import {AppComponent, SearchComponent, ReportComponent, QueryService, RedirectService, ReportService, environment} from './app/';
 import {HTTP_PROVIDERS} from '@angular/http';
 import {
