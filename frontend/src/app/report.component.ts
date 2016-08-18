49136be94699dfc104dde35429aa349d08034ee2
Aashil <aashil@devinci.io>
2016-08-18 13:09:22 -0400

    PROD-230: Remove bootstrap cdn from ReportComponent since it is globally loaded in the index.html page.

--- frontend/src/app/report.component.ts
+++ frontend/src/app/report.component.ts
@@ -9,8 +9,6 @@ import { AngularFire } from 'angularfire2';
 @Component({
     selector: 'report',
     templateUrl: './app/report.component.html',
-    styleUrls: ['https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" ' +
-    'integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u'],
     providers: [QueryService]
 })
 
