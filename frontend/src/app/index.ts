526314e7c72f8dcf7021992234e56eb5ce93ab78
Aashil <aashil@devinci.io>
2016-08-18 17:38:43 -0400

    PROD-230: Create the ReportComponent which renders the latest queries to the page.
    - Talks to the backend via QueryService.
    - Also listens for a user logging in to send the request.

--- frontend/src/app/index.ts
+++ frontend/src/app/index.ts
@@ -2,4 +2,5 @@ export * from './environment';
 export * from './app.component';
 export * from './search.component';
 export * from './query.service';
+export * from './report.component';
 export * from './redirect.service';
---
+++ frontend/src/app/report.component.ts
@@ -0,0 +1,36 @@
+/**
+ * Created by aashil on 8/12/16.
+ */
+
+import { Component, OnInit } from '@angular/core';
+import { QueryService } from './query.service';
+import { AngularFire } from 'angularfire2';
+
+@Component({
+    selector: 'report',
+    templateUrl: './app/report.component.html',
+    providers: [QueryService]
+})
+
+export class ReportComponent implements OnInit {
+
+    private data: any;
+
+    constructor(private queryService: QueryService, private af: AngularFire) {}
+
+    ngOnInit() {
+
+        this.af.auth.subscribe(
+            auth => {
+                if (auth != null) {
+                    this.queryService.getQueries().subscribe(
+                        data => {
+                            this.data = data;
+                        }, error => { console.log('Error happened: ' + error);
+                        }
+                    );
+                }
+            }
+        );
+    }
+}
