ed9d7c9f60e0d5daee6ec06261f85fac4027f6ba
Frank Carey <frank@devinci.io>
2016-08-19 14:47:39 -0400

    PROD-237: Add google analytics and set the UID in GA when a user logs in.

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,14 +2,30 @@ import { Component } from '@angular/core';
 import { SearchComponent } from './search.component';
 import { ReportComponent } from './report.component';
 import { AuthButtonComponent } from './auth.button.component';
+import { AngularFireAuth } from 'angularfire2';
 
+// Declare ga function as ambient
+declare var ga: Function;
 
 @Component({
-  moduleId: module.id,
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
-  directives: [SearchComponent, ReportComponent, AuthButtonComponent]
+    moduleId: module.id,
+    selector: 'app-root',
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css'],
+    directives: [SearchComponent, ReportComponent, AuthButtonComponent]
 })
 export class AppComponent {
+
+    constructor (private auth: AngularFireAuth) {
+
+        // Subscribe to the authentication events of angularfire2 so we can update
+        // the UID on Google Analytics.
+        this.auth.subscribe(
+            authEvent => {
+                if (authEvent != null) {
+                    ga('set', 'userId', authEvent.uid); // Set the user ID using signed-in user_id.
+                }
+            }
+        );
+    }
 }
