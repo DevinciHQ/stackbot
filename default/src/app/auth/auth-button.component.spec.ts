04844ce370493698f3512cb2db4282d9725dc57e
Aashil <aashil@devinci.io>
2016-08-23 18:11:58 -0400

    PROD-255: Write test to see if AuthService's login function was called.

---
+++ default/src/app/auth/auth-button.component.spec.ts
@@ -0,0 +1,56 @@
+/**
+ * Created by aashil on 8/23/16.
+ */
+
+
+import {
+  describe,
+  expect,
+  addProviders,
+  it,
+  inject,
+} from '@angular/core/testing';
+
+import { AuthService } from './auth.service';
+import { AuthButtonComponent } from './auth-button.component';
+import {Subject}    from 'rxjs/Subject';
+
+
+class MockAuthService {
+
+   private token = new Subject<string>();
+
+    // Observable string streams
+    // user$ = this.user.asObservable();
+    tokenEvent$ = this.token.asObservable();
+
+    login() {
+    }
+
+    logout() {
+
+    }
+
+    authEvent() {
+        return this.tokenEvent$;
+    }
+
+}
+
+describe('AuthButtonComponent', () => {
+    beforeEach(() => {
+        addProviders([
+            AuthButtonComponent,
+            {provide: AuthService, useClass: MockAuthService}
+        ]);
+    });
+
+    it('should call the login function of the AuthService', inject(
+                // Note that we pass XHRBackend, NOT MockBackend.
+                [AuthButtonComponent, AuthService], (authButtonComponent: AuthButtonComponent, auth: MockAuthService) => {
+
+        spyOn(auth, 'login');
+        authButtonComponent.login();
+        expect(auth.login).toHaveBeenCalled();
+    }));
+});
