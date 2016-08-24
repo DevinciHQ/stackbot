d08f1975bf8cb8a9d65d641ca312505ba2b732f0
Frank Carey <frank@devinci.io>
2016-08-24 15:39:45 -0400

    fixup! PROD-246: Implement webpack to club all the dependencies and code in a single file leading to significant reduction in the time to complete all the requests.

--- default/config/webpack.dev.js
+++ default/config/webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
 
   output: {
-    path: helpers.root('webpack-dist'),
+    path: helpers.root('dist'),
     publicPath: 'http://localhost:8080/',
     filename: '[name].js',
     chunkFilename: '[id].chunk.js'
