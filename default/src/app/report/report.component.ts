6aa70f79784050863ce884b5ce528ed923969d3f
Aashil <aashil@devinci.io>
2016-08-25 16:43:22 -0400

    PROD-259: Fix the bug where the table headers of the report where still showing when the user logged out.

--- default/src/app/report/report.component.ts
+++ default/src/app/report/report.component.ts
@@ -43,7 +43,7 @@ export class ReportComponent {
                         }
                     );
                 } else {
-                    this.data = [];
+                    this.data = null;
                 }
             },
             err => {
