378e9e7db3578cc55ffd6c75a9f3923b039a673f
Frank Carey <frank@devinci.io>
2016-08-25 17:15:24 -0400

    PROD-246: Update karma config and test-shim for use with webpack.
    - Based on the official documentation from https://angular.io/docs/ts/latest/guide/webpack.html

--- default/config/karma.conf.js
+++ default/config/karma.conf.js
@@ -1,57 +1,37 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
+var webpackConfig = require('./webpack.test');
 
 module.exports = function (config) {
-  config.set({
-    basePath: '..',
+  var _config = {
+    basePath: '',
+
     frameworks: ['jasmine'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-junit-reporter')
-    ],
-    customLaunchers: {
-      // chrome setup for travis CI using chromium
-      Chrome_travis_ci: {
-        base: 'Chrome',
-        flags: ['--no-sandbox']
-      }
-    },
+
     files: [
-      { pattern: 'dist/vendor/core-js/client/shim.min.js', included: true, watched: false },
-      { pattern: 'dist/vendor/zone.js/dist/zone.js', included: true, watched: false },
-      { pattern: 'dist/vendor/reflect-metadata/Reflect.js', included: true, watched: false },
-      { pattern: 'dist/vendor/systemjs/dist/system-polyfills.js', included: true, watched: false },
-      { pattern: 'dist/vendor/systemjs/dist/system.src.js', included: true, watched: false },
-      { pattern: 'dist/vendor/zone.js/dist/async-test.js', included: true, watched: false },
-      { pattern: 'dist/vendor/zone.js/dist/fake-async-test.js', included: true, watched: false },
-
-      { pattern: 'config/karma-test-shim.js', included: true, watched: true },
-
-      // Distribution folder.
-      { pattern: 'dist/**/*', included: false, watched: true }
+      {pattern: './karma-test-shim.js', watched: false}
     ],
-    exclude: [
-      // Vendor packages might include spec files. We don't want to use those.
-      'dist/vendor/**/*.spec.js'
-    ],
-    preprocessors: {},
-    reporters: ['progress'],
-    // the default configuration
-    junitReporter: {
-      outputDir: process.env.JUNIT_DIR || '', // results will be saved as $outputDir/$browserName.xml
-      outputFile: process.env.JUNIT_FILE || undefined, // if included, results will be saved as $outputDir/$browserName/$outputFile
-      suite: '', // suite will become the package name attribute in xml testsuite element
-      useBrowserName: false, // add browser name to report and classes names
-      nameFormatter: undefined, // function (browser, result) to customize the name attribute in xml testcase element
-      classNameFormatter: undefined, // function (browser, result) to customize the classname attribute in xml testcase element
-      properties: {} // key value pair of properties to add to the <properties> section of the report
+
+    preprocessors: {
+      './karma-test-shim.js': ['webpack', 'sourcemap']
     },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
+    autoWatch: false,
+    browsers: ['PhantomJS'],
     singleRun: true
-  });
+  };
+
+  config.set(_config);
 };
