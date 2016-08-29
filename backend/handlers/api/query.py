0fb27c38f341909796e560c2f77e5f7477aa7d98
Frank Carey <frank@devinci.io>
2016-08-29 14:27:36 -0400

    PROD-243: Upgrade the query api handler to flask.

--- backend/handlers/api/query.py
+++ backend/handlers/api/query.py
@@ -1,71 +1,72 @@
-import webapp2
 import datetime
 import json
-import urllib
-import security
 import logging
-from user_agents import parse as parseUA
+import urllib
 
+from flask import request, abort
+from shared import app
 from models.query import Query
+from shared import security
+from user_agents import parse as parseUA
 
 
 # This handles the incoming request and creates a Query entity if a query string
 # was passed.
-class QueryHandler(webapp2.RequestHandler):
+@app.route('/api/q', methods=['GET'])
+def query_handler():
+    # Set CORS headers for GET requests
+    # security.set_cors_header(self)
+    # Make sure the length of the query string is at least 1 char.
+    q = request.args.get('q', '')
+    if len(q) <= 0:
+        abort(400)
 
-    def options(self):
-        # Set CORS headers for non-GET requests (json data POSTS)
-        security.set_cors_header(self)
+    user_id = None
+    try:
+        user_id = security.verify_jwt_token(request).get('sub')
+    except security.ValidationError as err:
+        # IF the user isn't logged in, then throw a 403 error.
+        logging.error(err)
+        abort(403)
 
-    def get(self):
-        # Set CORS headers for GET requests
-        security.set_cors_header(self)
-        # Make sure the length of the query string is at least 1 char.
-        q = self.request.get('q')
-        if len(q) > 0:
-            payload = security.verify_request(self.request)
-            user_id = payload.get('sub')
-            if not user_id:
-                raise Exception("sub is missing from token payload.")
+    # Get the user-agent header from the request.
+    userAgent = parseUA(request.headers['User-Agent'])
+    geo = get_geo_data(request)
 
-            # Get the user-agent header from the request.
-            userAgent = parseUA(self.request.headers['User-Agent'])
-            geo = get_geo_data(self.request)
+    # Create a new Query entity from the q value.
+    # TODO: Add the other values that we want from the request headers.
+    query = Query(
+        query=q,
+        os=str(userAgent.os.family) + " Version: " + str(userAgent.os.version_string),
+        browser=str(userAgent.browser.family),
+        timestamp=datetime.datetime.utcnow().isoformat(),
+        country=geo['country'],
+        city=geo['city'],
+        city_lat_long=geo['city_lat_long'],
+        ip=request.remote_addr,
+        uid=user_id
+    )
+    # Save to the datatore.
+    query.put()
+    # Output some debug messages for now.
+    # TODO: Redirect to google.
+    logging.info('Saved')
+    logging.debug('query: %s', str(q))
 
-            # Create a new Query entity from the q value.
-            # TODO: Add the other values that we want from the request headers.
-            query = Query(
-                query=q,
-                os=str(userAgent.os.family) + " Version: " + str(userAgent.os.version_string),
-                browser=str(userAgent.browser.family),
-                timestamp=datetime.datetime.utcnow().isoformat(),
-                country=geo['country'],
-                city=geo['city'],
-                city_lat_long=geo['city_lat_long'],
-                ip=self.request.remote_addr,
-                uid=user_id,
-                source=self.request.get('source', None)
-            )
-            # Save to the datatore.
-            query.put()
-            # Output some debug messages for now.
-            # TODO: Redirect to google.
-            logging.info('Saved')
-            logging.debug('query: %s', str(q))
+    escaped_q = urllib.urlencode({'q': q})
+    redirect = 'http://google.com/#' + escaped_q
 
-            escaped_q = urllib.urlencode({'q': q})
-            redirect = 'http://google.com/#' + escaped_q
+    # Output for when we first land on the page (or when no query was entered)
+    # response.headers['Content-Type'] = 'application/json'
+    payload = {
+        'redirect': redirect
+    }
+    output = {
+        'success': True,
+        'payload': payload,
+    }
+    return json.dumps(output)
 
-            # Output for when we first land on the page (or when no query was entered)
-            self.response.headers['Content-Type'] = 'application/json'
-            payload = {
-                'redirect': redirect
-            }
-            output = {
-                'success': True,
-                'payload': payload,
-            }
-            self.response.out.write(json.dumps(output))
 
 # Not that geoip2 (from maximind) doesn't work on GAE because there is a C lib in there apparently.
 # We can use Appengine's added headers to do that work though thankfully.
