d6cf9377d8c4dc3a259c43f80b217c54679bf468
Aashil <aashil@devinci.io>
2016-08-29 17:39:23 -0400

    PROD-243: Fix the query.service.spec test.
    * Take advantage of observables from rxjs.
    * Class has full coverage.
    * see: http://reactivex.io/rxjs/manual/overview.html#observable

--- default/src/app/query/query.service.spec.ts
+++ default/src/app/query/query.service.spec.ts
@@ -2,73 +2,91 @@
 import {
   inject,
   addProviders,
-  async
+  async,
 } from '@angular/core/testing';
-import { HTTP_PROVIDERS, ConnectionBackend, BaseRequestOptions, XHRBackend, Response, ResponseOptions, Http} from '@angular/http';
-import { MockBackend, MockConnection} from '@angular/http/testing';
 import { QueryService } from './query.service';
-import { AuthHttp } from 'angular2-jwt';
+import { BackendService} from '../shared/backend.service';
+import { Observable, Observer } from 'rxjs';
 
-class AuthBackend extends MockBackend {
 
+class RequestParams {
+    constructor(public path: string, public data: any) {}
 }
 
+class MockBackendService {
+
+    request(path: string, data: any) {
+        return Observable.create(
+            (observer: Observer<RequestParams>) => {
+                // observer.next(new RequestParams(path, data));
+                observer.next(null);
+                // observer.complete();
+            }
+        );
+    }
+
+}
 describe('QueryService', () => {
     beforeEach(() => {
         addProviders([
             QueryService,
-            MockBackend,
-            BaseRequestOptions,
-            HTTP_PROVIDERS,
-            // This is how you can Mock the backend of Http so that you can actually intercept and provide your own
-            // custom responses. See examples below.. This took hours to figure out as there isn't great documentation.
-            // addProviders, async, and inject were all added in angular RC4 (alternatives existed earlier).
-            {provide: AuthHttp, useClass: Http},
-            {provide: XHRBackend, useClass: MockBackend},
-            {provide: ConnectionBackend, useClass: MockBackend}
+            {provide: BackendService, useClass: MockBackendService}
         ]);
     });
 
-    it('should get back a redirect when sending a query', async(inject(
-                // Note that we pass XHRBackend, NOT MockBackend.
-                [QueryService, XHRBackend], (queryService: QueryService, backend: MockBackend) => {
-        const mockedResponse = new ResponseOptions({
-            body: '{"success": true, "payload": {"redirect": "http://google.com/#q=asdf"}}',
-            status: 200
-        });
+    it('should call backend path with query and source as data.', async(inject(
+        // Note that we pass XHRBackend, NOT MockBackend.
+        [QueryService, BackendService], (queryService: QueryService, backend: MockBackendService) => {
 
-        backend.connections.subscribe((connection: MockConnection) => {
-            let url = connection.request.url.split('?')[0];
-            if (url.endsWith('api/q')) {
-                connection.mockRespond(new Response(mockedResponse));
+            let query = 'asdf';
+            let source = 'test';
+            let path = '/api/q';
+
+            spyOn(backend, 'request').and.callThrough();
+
+            queryService.doQuery('asdf', 'test').subscribe(data => {
+                expect(backend.request).toHaveBeenCalledWith('/api/q', {q: query, source: source});
+                },
+                err => {
+                    throw new Error('Query failed');
+                }
+            );
+
+        }
+    )));
+    describe('getQueries()', () => {
+
+        it('should call backend.request() just the /api/report path and an empty object.', async(inject(
+            // Note that we pass XHRBackend, NOT MockBackend.
+            [QueryService, BackendService], (queryService: QueryService, backend: MockBackendService) => {
+
+                spyOn(backend, 'request').and.callThrough();
+
+                queryService.getQueries().subscribe(data => {
+                        expect(backend.request).toHaveBeenCalledWith('/api/report', {});
+                    },
+                    err => {
+                        throw new Error('Query failed');
+                    }
+                );
 
             }
-        });
-
-        // Ensure that doQuery gets back a redirect.
-        queryService.doQuery('asdf', 'test').subscribe( data => {
-                expect(data).toEqual({'redirect': 'http://google.com/#q=asdf'});
-            },
-            err => {
-                throw new Error('Query failed');
-            }
-
-        );
-
-    })));
-
-
-    it('should urlencode data when sending a query', async(inject(
-                // Note that we pass XHRBackend, NOT MockBackend.
-                [QueryService, XHRBackend], (queryService: QueryService, backend: MockBackend) => {
-
-        backend.connections.subscribe((connection: MockConnection) => {
-            let getParam = connection.request.url.split('?')[1];
-            expect(getParam).toEqual('q=%26%2B%3D%23%22%20_!a1');
-        });
-
-        // Ensure that doQuery gets back a redirect.
-        queryService.doQuery('&+=#" _!a1', 'test');
-
-    })));
+        )));
+    });
 });
+
+
+//     it('should urlencode data when sending a query', async(inject(
+//                 // Note that we pass XHRBackend, NOT MockBackend.
+//                 [QueryService, XHRBackend], (queryService: QueryService, backend: MockBackend) => {
+//
+//         backend.connections.subscribe((connection: MockConnection) => {
+//             let getParam = connection.request.url.split('?')[1];
+//             expect(getParam).toEqual('q=%26%2B%3D%23%22%20_!a1');
+//         });
+//
+//         // Ensure that doQuery gets back a redirect.
+//         queryService.doQuery('&+=#" _!a1', 'test');
+//
+//     })));
+// });
