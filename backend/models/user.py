eaaab0325d3cce2a4c8b036aa00e73df2cd7cc08
Frank Carey <frank@devinci.io>
2016-08-29 14:37:33 -0400

    PROD-243: Update User model to where we specify the fields like created, user_id, and email.

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,7 +1,15 @@
 # For use when dealing with the datastore.
 from google.appengine.ext import ndb
 
-# A simple Datastore Entity that takes any values (good for development)
-# See https://cloud.google.com/appengine/docs/python/ndb/creating-entity-models
-class User(ndb.Expando):
-    pass
+class User(ndb.Model):
+
+    user_id = ndb.StringProperty(required=True)
+    username = ndb.StringProperty(required=True)
+    email = ndb.StringProperty(required=True)
+    email_verified = ndb.BooleanProperty(default=False)
+    #first_name = ndb.StringProperty(required=True)
+    #last_name = ndb.StringProperty(required=True)
+    picture = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
+
