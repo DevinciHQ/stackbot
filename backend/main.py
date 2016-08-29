563aae21b5181a5e6b201bfb75e2ff5645360220
Frank Carey <frank@devinci.io>
2016-08-29 14:29:12 -0400

    PROD-243: Upgrade the main.py to use flask and clean it up.

--- backend/main.py
+++ backend/main.py
@@ -1,37 +1,37 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+import os
 
-import webapp2
-import logging, os
-from  handlers import report, query
+from flask import request
 
+from shared import app
+from shared import security
+
+
+# Load all the handlers from ./handlers.__init___
+# noinspection PyUnresolvedReferences
+import handlers
+
+app.debug = True
+app.secret_key = 'development'
 
 """ ------------- MAIN ------------------ """
 
-logging.debug('SERVER_SOFTWARE', os.getenv('SERVER_SOFTWARE', ''))
+server = str(os.getenv('SERVER_SOFTWARE', ''))
+# logging.info("Running using server: ", env)
 # Set a flag for what environment we're in.
-if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+if server.startswith('Google App Engine/'):
     # Running on Google AppEngine
     env = 'production'
 else:
     # Running locally using dev_appserver.py
     env = 'local'
 
-# Actually run the webserver and accept requests.
-app = webapp2.WSGIApplication([
-    ('/api/q', query.QueryHandler),
-    ('/api/report', report.ReportHandler),
-], debug=True)
 
+@app.after_request
+def after_request(response):
+    """ This handles CORS requests for all requests (including OPTIONS).  """
+    response = security.set_cors_header(request, response)
+    return response
+
+# Note: We don't need to call run() since our application is embedded within
+# the App Engine WSGI application server.
 
