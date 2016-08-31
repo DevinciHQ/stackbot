b79a217343871b34aa8b146df9ef3cdfe5908a62
Aashil <aashil@devinci.io>
2016-08-31 16:28:20 -0400

    PROD-255: Write the test to get the public key back from the cert having particular kid.
    * Create a local public key file for us to check the retrieved public key.
    * Create a local file with certs for us to use it instead of querying firebase.

--- backend/tests/security_test.py
+++ backend/tests/security_test.py
@@ -1,7 +1,7 @@
 """ Test the Query model."""
 import unittest
 import json
-import datetime
+import time
 
 from google.appengine.ext import ndb, testbed
 from shared.security import PUBKEY
@@ -29,18 +29,6 @@ class DatastoreTestCase(unittest.TestCase):
         # the tests. See: http://stackoverflow.com/a/31975818
         self.testbed.init_urlfetch_stub()
 
-        with open('./test_data/firebase.certs.json') as data_file:
-            security.PUBKEY._keys = json.load(data_file)
-
-        now = datetime.datetime.now()
-        hour = now.hour + 1
-        now.replace(hour)
-        security.PUBKEY._expires = now
-
-
-
-
-    # [END datastore_example_test]
 
     # [START datastore_example_teardown]
     def tearDown(self):
@@ -50,11 +38,32 @@ class DatastoreTestCase(unittest.TestCase):
 
     def testRefresh(self):
         """ Test if the cache holding the keys is refreshed. """
+        self.removeFakeCerts()
         self.assertDictEqual(PUBKEY._keys, {})
         PUBKEY.refresh()
         self.assertIsNot(PUBKEY._keys, {})
 
-
     def testGetPublicCerts(self):
-        """Test to see if we get back the public cert back."""
-        security.PUBKEY.get_public_cert("6f83ab6e516e718fba9ddeb6647fd5fb752a151b")
+        """Test to check if we get back the public key back."""
+        self.setFakeCerts()
+        pkey = None
+        # Obtain the CERT from the kid passed in which is converted into a public key and
+        # returned to us.
+        pkey = security.PUBKEY.get_public_cert("fc2da7fa53d92e3bcba8a17e74b34da9dd585065")
+        self.assertIsNotNone(pkey)
+        pub = open('./tests/test_data/public_key.txt', 'r').read()
+        # This tests if the public key returned is same as what we have on the file.
+        self.assertEqual(pkey, pub)
+
+    def setFakeCerts(self):
+        """Set the fake CERTS from the local file"""
+        with open('./tests/test_data/firebase.certs.json') as data_file:
+            security.PUBKEY._keys = json.load(data_file)
+        now = time.time() + 60 * 60 * 24
+        future = time.gmtime(now)
+        security.PUBKEY._expires = future
+
+    def removeFakeCerts(self):
+        """Remove the fake CERTS and set the expiration time to None"""
+        security.PUBKEY._keys = {}
+        security.PUBKEY._expires = None
