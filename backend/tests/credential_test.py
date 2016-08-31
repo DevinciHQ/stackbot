5816304167eb803dadf46d2ad41d2f6d2e961122
Aashil <aashil@devinci.io>
2016-08-31 09:30:53 -0400

    PROD-243: Create a stub for testing Credentials data stored in the datastore.

---
+++ backend/tests/credential_test.py
@@ -0,0 +1,33 @@
+""" Test the Credential model."""
+import unittest
+
+from google.appengine.ext import ndb, testbed
+
+from models.credential import Credential
+
+# [START datastore_example_test]
+class DatastoreTestCase(unittest.TestCase):
+    """ Create a test case. """
+
+    def setUp(self):
+        """ Setup the test."""
+        # First, create an instance of the Testbed class.
+        self.testbed = testbed.Testbed()
+        # Then activate the testbed, which prepares the service stubs for use.
+        self.testbed.activate()
+        # Next, declare which service stubs you want to use.
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        # Clear ndb's in-context cache between tests.
+        # This prevents data from leaking between tests.
+        # Alternatively, you could disable caching by
+        # using ndb.get_context().set_cache_policy(False)
+        ndb.get_context().clear_cache()
+
+# [END datastore_example_test]
+
+    # [START datastore_example_teardown]
+    def tearDown(self):
+        """ Tear down the test. """
+        self.testbed.deactivate()
+    # [END datastore_example_teardown]
