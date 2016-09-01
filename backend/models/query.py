e3b0db5526af46b63fa6e6219ef881ca2226a044
Aashil <aashil@devinci.io>
2016-09-01 10:50:35 -0400

    PROD-255: Define the columns of the Query table instead of keeping it generic.
    * Now is the time to move from generic to strongly defined columns.

--- backend/models/query.py
+++ backend/models/query.py
@@ -2,9 +2,18 @@
 # For use when dealing with the datastore.
 from google.appengine.ext import ndb
 
-# A simple Datastore Entity that takes any values (good for development)
 # See https://cloud.google.com/appengine/docs/python/ndb/creating-entity-models
-class Query(ndb.Expando):
-    """Let the schema of Query table be generic as of now.
-       We need to change this in future."""
-    pass
+class Query(ndb.Model):
+    """Define the columns for our Query table"""
+    query = ndb.StringProperty(required=True)
+    os = ndb.StringProperty()
+    browser = ndb.StringProperty()
+    timestamp = ndb.StringProperty()
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
+    country = ndb.StringProperty()
+    city = ndb.StringProperty()
+    city_lat_long = ndb.StringProperty()
+    ip = ndb.StringProperty()
+    uid = ndb.StringProperty()
+    source = ndb.StringProperty()
