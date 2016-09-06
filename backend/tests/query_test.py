dadb5b884dae36672c359a92fbd527ca5265fc6e
Aashil <aashil@devinci.io>
2016-09-06 17:43:32 -0400

    PROD-266: Create tests for:
    * Checking if we can get the correct tags from the search_string.
    * Checking if we can get the query from the search_string.

--- backend/tests/query_test.py
+++ backend/tests/query_test.py
@@ -6,6 +6,7 @@ from google.appengine.ext import ndb, testbed
 from models.query import Query
 
 from context import get_fake_jwt_token, remove_fake_certs, set_fake_certs, setup_fake_user
+import handlers.api.query as query
 
 # [START datastore_example_test]
 class DatastoreTestCase(unittest.TestCase):
@@ -43,3 +44,29 @@ class DatastoreTestCase(unittest.TestCase):
         """ Test the Query can be inserted. """
         test_query = Query(query="google", ip='127.0.0.1', source='site-search', user=self.user.key).put()
         self.assertEqual("google", test_query.get().query)
+
+    def test_query_tag(self):
+        """ Test to see if we can insert the tag in the Query and retrieve it. """
+        test_query = Query(query="google", ip='127.0.0.1', source='site-search', user=self.user.key, tags=['test']).put()
+        self.assertEqual(["test"], test_query.get().tags)
+
+    def test_hashtag(self):
+        """ Testing the hashtag feature."""
+        # Test to check if hashtag starts with a '#' symbol.
+        tags = query.get_tags("#this is a test")
+        self.assertEqual(tags, ["this"])
+
+        # Test to see if multiple hash tags work.
+        tags = query.get_tags("#this-is #testing the hashtag")
+        self.assertEqual(tags, ["this-is", "testing"])
+
+        # Test to not include a hashtag if it is not at the beginning of the query
+        tags = query.get_tags("#this-is for #testing the hashtag")
+        self.assertEqual(tags, ["this-is"])
+
+    def test_remove_hashtags(self):
+        """ Test to remove the hashtags out of the search query for storing it
+        in the query column."""
+
+        search_query = query.get_query("#this is a #test")
+        self.assertEqual(search_query, "is a #test")
