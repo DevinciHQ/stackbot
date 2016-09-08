1b883931a4af3222d5950c25634f1e8947532149
Aashil <aashil@devinci.io>
2016-09-08 12:15:38 -0400

    PROD-239: Add a feature in the backend to send only a limited number of results using cursor.
    * Accept a limit GET parameter to fetch the number of result specified in it.
    * Return the cursor as a part of the response. Cursor can either be a value or None (if the end of the datastore is reached)
    * Change the structure of response. We now send a 'cursor' field apart from 'payload' and 'success'.
    * More about cursors: https://cloud.google.com/appengine/docs/python/ndb/queries#cursors

--- backend/handlers/api/report.py
+++ backend/handlers/api/report.py
@@ -3,6 +3,7 @@ import logging
 
 from flask import request, abort, jsonify
 from shared import app, ApiResponse, security
+from google.appengine.datastore.datastore_query import Cursor
 
 # For use when dealing with the datastore.
 from google.appengine.ext import ndb
@@ -21,14 +22,25 @@ def report_handler():
         logging.debug(err)
         abort(401)
 
+    # Allow the frontend (and our tests) to request a limit of less than 100 items.
+    limit = int(request.args.get('limit', 0))
+    if not limit or limit > 100:
+        limit = 100
+
+
+    # We use the cursor feature of GAE to take note of the last data fetched from the database.
+    # See: https://cloud.google.com/appengine/docs/python/ndb/queries#cursors
+    cursor = Cursor(urlsafe=request.args.get('cursor', None))
+
     # https://cloud.google.com/appengine/docs/python/ndb/queries#properties_by_string
     # https://cloud.google.com/appengine/docs/python/ndb/queries#cursors
     # We are fetching all the columns instead of the specific columns because of a bug
     # (we believe it's a bug) where the result contains duplicate records for each of the
     # tags in the tags list. This issue(or a functionality) which is caused by the 'repeated'
     # property of a column is not documented on the GAE's site.
-    result = ndb.gql("SELECT * FROM Query WHERE uid = :1 ORDER BY "
-                     "timestamp DESC LIMIT 100", user.user_id)
+    result, next_cursor, more = ndb.gql("SELECT * FROM Query WHERE uid = :1 ORDER BY "
+                     "timestamp DESC", user.user_id).fetch_page(limit, start_cursor=cursor)
+
     data = []
     for query in result:
         # This is annoying.. maybe we should use another word instead of query?
@@ -45,4 +57,9 @@ def report_handler():
         }
         data.append(query_out)
 
-    return jsonify(ApiResponse(data))
+    # If there are more items and we have a cursor, then return it.
+    if more and next_cursor:
+        next_cursor = next_cursor.urlsafe()
+    else:
+        next_cursor = None
+    return jsonify(ApiResponse(payload=data, cursor=next_cursor))
