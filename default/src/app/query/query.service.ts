7500c1795edc36b32736a2102c6e1ef8fbe2e4f1
Frank Carey <frank@devinci.io>
2016-09-13 14:17:58 -0400

    PROD-269: Fix query service so that it chooses to send an authenticated or unauthenticated request.
    - If the user is pending (null) then it doesn't do anything at all.

--- default/src/app/query/query.service.ts
+++ default/src/app/query/query.service.ts
@@ -3,19 +3,35 @@ import { Injectable }     from '@angular/core';
 import { Observable }     from 'rxjs/Observable';
 import 'rxjs/add/operator/catch';
 import {BackendService} from '../shared/backend.service';
+import {AuthService} from '../auth/auth.service';
 
 @Injectable()
 export class QueryService {
-    constructor(private backend: BackendService) {}
+    constructor(private backend: BackendService, private auth: AuthService) {}
 
     /**
      *
      * @param query
+     * @param source
      * @returns {Observable<Object>}
      */
     public doQuery(query: string, source: string): Observable<Object> {
+        let user = this.auth.getUser().getValue();
+        let use_auth: boolean;
+
+        // Send an authenticated request if the user is logged in.
+        if (user && user.loggedIn) {
+            use_auth = true;
+        } else if (user && !user.loggedIn) {
+            // Send an un-authenticated request if the user is logged out.
+            use_auth = false;
+        } else {
+            // If the user is still pending (null), then don't do anything yet.
+            console.log('Not making any request until the user\'s loggedIn status is resolved.');
+            return;
+        }
         // Save the search query and get the JSON response (which also contains link to redirect) in return.
-        return this.backend.request('/api/q', {q: query, source: source });
+        return this.backend.request('/api/q', {q: query, source: source }, use_auth);
     }
 
     // Get the search history as a JSON response.
